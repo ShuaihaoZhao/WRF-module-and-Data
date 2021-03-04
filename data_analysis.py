ede7d7aac28d8fd16b04a6965c841834edf65952
Hao <526312824@qq.com>
2021-03-04 09:57:40 -0700

    Fix wind speed variable
    as title

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,7 @@
 
 #Compare the foracasted data with the real weather data
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
@@ -44,38 +45,33 @@ part_forecasted=forecasted_data[['Temperature[Degree Celsius]','Surface Pressure
 #plt.show()
 
 #######################################################################Wind
-wind=[]
 
-for index_f,rows in part_forecasted.iterrows():
-    wind.append((math.sqrt(part_forecasted.iloc[index_f,2]**2+part_forecasted.iloc[index_f,3]**2)/1000)*3600)
+part_forecasted['Wind']=((np.sqrt(part_forecasted['10m_X_Wind_Speed [m/s]']**2+part_forecasted['10m_Y_Wind_Speed [m/s]']**2)/1000.0)*3600)
 
-part_forecasted('Wind')=wind
-#df_1=pd.DataFrame(wind,col)
+plt.figure(figsize=(20,10))
+plt.plot(forecasted_data.iloc[:,0],forecasted_data.iloc[:,4],label='forecasted')
+plt.plot(forecasted_data.iloc[:,0],part_real.iloc[:,2],label='real');
 
-#plt.figure(figsize=(20,10))
-#plt.plot(forecasted_data.iloc[:,0],wind[:,0],label='forecasted')
-#plt.plot(forecasted_data.iloc[:,0],part_real.iloc[:,2],label='real');
-#
-#plt.title('Real Wind Speed vs Forecasted Wind Speed')
-#plt.xlabel('Time')
-#plt.ylabel('Wind Speed [km/h]')
-#plt.legend(loc="upper left")
-#plt.show()
+plt.title('Real Wind Speed vs Forecasted Wind Speed')
+plt.xlabel('Time')
+plt.ylabel('Wind Speed [km/h]')
+plt.legend(loc="upper left")
+plt.show()
     
 #######################################################################    
-#counter=0
-#error=0#temperature
-#error_1=0#pressure
-#error_2=0#wind Using Relative Percent Difference
-#for index,rows in part_real.iterrows():
-#    error=error+((part_forecasted.iloc[index,0]+273.15)-(part_real.iloc[index,0]+273.15))/(part_real.iloc[index,0]+273.15)
-#    error_1=error_1+abs((part_forecasted.iloc[index,1])-(part_real.iloc[index,1])*1000)/(part_real.iloc[index,1]*1000)
-#    error_2=error_2+2*abs((wind[index,0])-(part_real.iloc[index,2]))/((part_real.iloc[index,2])+(wind[index,0]))
-#    counter+=1
-#    
-#print('Temperature mean percentage error: ',"%.4f" % round(error/counter, 4))
-#print('Pressure mean percentage error: ',"%.4f" % round(error_1/counter, 4))
-#print('Wind speed mean percentage error: ',"%.4f" % round(error_2/counter, 4))
+counter=0
+error=0#temperature
+error_1=0#pressure
+error_2=0#wind Using Relative Percent Difference
+for index,rows in part_real.iterrows():
+    error=error+((part_forecasted.iloc[index,0]+273.15)-(part_real.iloc[index,0]+273.15))/(part_real.iloc[index,0]+273.15)
+    error_1=error_1+abs((part_forecasted.iloc[index,1])-(part_real.iloc[index,1])*1000)/(part_real.iloc[index,1]*1000)
+    error_2=error_2+2*abs((forecasted_data.iloc[index,4])-(part_real.iloc[index,2]))/((part_real.iloc[index,2])+(forecasted_data.iloc[index,4]))
+    counter+=1
+    
+print('Temperature mean percentage error: ',"%.4f" % round(error/counter, 4))
+print('Pressure mean percentage error: ',"%.4f" % round(error_1/counter, 4))
+print('Wind speed mean percentage error: ',"%.4f" % round(error_2/counter, 4))
 #######################################################################
 
 
