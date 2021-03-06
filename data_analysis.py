85be58f2ba68f0cfee2dfe1af1958a7d7acbfbff
Hao <526312824@qq.com>
2021-03-06 16:02:48 -0700

    Update
    1. add a sample output file
    2. Try to get full data through Wget using Cygwin terminal but data is still not complete (missing solar irradiation data)
    3. Correlation not clear yet

--- data_analysis.py
+++ data_analysis.py
@@ -22,40 +22,40 @@ part_forecasted=forecasted_data[['Temperature[Degree Celsius]','Surface Pressure
                                  
 #######################################################################Temperature
 
-#plt.figure(figsize=(20,10))
-#plt.plot(forecasted_data.iloc[:,0],part_forecasted.iloc[:,0],label='forecasted')
-#plt.plot(forecasted_data.iloc[:,0],part_real.iloc[:,0],label='real');
-#
-#plt.title('Real Temperature vs Forecasted Temperature')
-#plt.xlabel('Time')
-#plt.ylabel('Temperature (Kelvin)')
-#plt.legend(loc="upper left")
-#plt.show()
+plt.figure(figsize=(20,10))
+plt.plot(forecasted_data.iloc[:,0],part_forecasted.iloc[:,0],label='forecasted')
+plt.plot(forecasted_data.iloc[:,0],part_real.iloc[:,0],label='real');
+
+plt.title('Real Temperature vs Forecasted Temperature')
+plt.xlabel('Time')
+plt.ylabel('Temperature (Kelvin)')
+plt.legend(loc="upper left")
+plt.show()
 #
 ########################################################################Pressure
-#plt.figure(figsize=(20,10))
-#plt.plot(forecasted_data.iloc[:,0],part_forecasted.iloc[:,1],label='forecasted')
-#plt.plot(forecasted_data.iloc[:,0],part_real.iloc[:,1]*1000,label='real');
-#
-#plt.title('Real Pressure vs Forecasted Pressure')
-#plt.xlabel('Time')
-#plt.ylabel('Pressure (Pa)')
-#plt.legend(loc="upper left")
-#plt.show()
+plt.figure(figsize=(20,10))
+plt.plot(forecasted_data.iloc[:,0],part_forecasted.iloc[:,1],label='forecasted')
+plt.plot(forecasted_data.iloc[:,0],part_real.iloc[:,1]*1000,label='real');
+
+plt.title('Real Pressure vs Forecasted Pressure')
+plt.xlabel('Time')
+plt.ylabel('Pressure (Pa)')
+plt.legend(loc="upper left")
+plt.show()
 #
 ########################################################################Wind
 ##
 part_forecasted['Wind[km/h]']=((np.sqrt(part_forecasted['10m_X_Wind_Speed [m/s]']**2+part_forecasted['10m_Y_Wind_Speed [m/s]']**2)/1000.0)*3600)
 ##
-##plt.figure(figsize=(20,10))
-##plt.plot(forecasted_data.iloc[:,0],part_forecasted.iloc[:,4],label='forecasted')
-##plt.plot(forecasted_data.iloc[:,0],part_real.iloc[:,2],label='real');
-##
-##plt.title('Real Wind Speed vs Forecasted Wind Speed')
-##plt.xlabel('Time')
-##plt.ylabel('Wind Speed [km/h]')
-##plt.legend(loc="upper left")
-##plt.show()
+plt.figure(figsize=(20,10))
+plt.plot(forecasted_data.iloc[:,0],part_forecasted.iloc[:,4],label='forecasted')
+plt.plot(forecasted_data.iloc[:,0],part_real.iloc[:,2],label='real');
+
+plt.title('Real Wind Speed vs Forecasted Wind Speed')
+plt.xlabel('Time')
+plt.ylabel('Wind Speed [km/h]')
+plt.legend(loc="upper left")
+plt.show()
 #    
 ########################################################################    
 part_forecasted['precipitation[mm]']=forecasted_data.iloc[:,5]+forecasted_data.iloc[:,6]
@@ -71,19 +71,23 @@ plt.legend(loc="upper left")
 plt.show()                                
                                  
 ########################################################################
-#counter=0
-#error=0#temperature
-#error_1=0#pressure
-#error_2=0#wind Using Relative Percent Difference
-#for index,rows in part_real.iterrows():
-#    error=error+abs((part_forecasted.iloc[index,0]+273.15)-(part_real.iloc[index,0]+273.15))/abs(part_real.iloc[index,0]+273.15)
-#    error_1=error_1+abs((part_forecasted.iloc[index,1])-(part_real.iloc[index,1])*1000)/(part_real.iloc[index,1]*1000)
-#    error_2=error_2+2*abs((part_forecasted.iloc[index,4])-(part_real.iloc[index,2]))/abs((part_real.iloc[index,2])+(part_forecasted.iloc[index,4]))
-#    counter+=1
-#    
-#print('Temperature mean percentage error: ',"%.4f" % round(error/counter, 4))
-#print('Pressure mean percentage error: ',"%.4f" % round(error_1/counter, 4))
-#print('Wind speed mean percentage error: ',"%.4f" % round(error_2/counter, 4))
+counter=0
+error=0#temperature
+error_1=0#pressure
+error_2=0#wind Using Relative Percent Difference
+error_3=0#precipitation
+for index,rows in part_real.iterrows():
+    error=error+abs((part_forecasted.iloc[index,0]+273.15)-(part_real.iloc[index,0]+273.15))/abs(part_real.iloc[index,0]+273.15)
+    error_1=error_1+abs((part_forecasted.iloc[index,1])-(part_real.iloc[index,1])*1000)/(part_real.iloc[index,1]*1000)
+    error_2=error_2+2*abs((part_forecasted.iloc[index,4])-(part_real.iloc[index,2]))/abs((part_real.iloc[index,2])+(part_forecasted.iloc[index,4]))
+#    error_3=error_3+2*abs((part_forecasted.iloc[index,5])-(part_real.iloc[index,3]))/abs((part_real.iloc[index,3])+(part_forecasted.iloc[index,5]))
+    counter+=1
+    
+print('Temperature mean percentage error: ',"%.4f" % round(error/counter, 4))
+print('Pressure mean percentage error: ',"%.4f" % round(error_1/counter, 4))
+print('Wind speed mean percentage error: ',"%.4f" % round(error_2/counter, 4))
+#print('precipitation mean percentage error: ',"%.4f" % round(error_3/counter, 4))
+
 #######################################################################
 
 
